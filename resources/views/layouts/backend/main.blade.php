d23dc3506bed7760591459c21707b7e9eb7eb00a
rsqpxlavs <contact.digprix@gmail.com>
2017-11-14 01:27:07 +0530

    meta csrf field added to main backend layout

--- resources/views/layouts/backend/main.blade.php
+++ resources/views/layouts/backend/main.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="robots" content="noindex, nofollow">
         <link rel="shortcut icon" href="{{ asset( 'assets/favicon.png' )}}">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         @yield('seo')
         <link rel="stylesheet" type="text/css" href="{{ asset( 'assets/backend/lib/stroke-7/style.css' )}}"/>
         <link rel="stylesheet" type="text/css" href="{{ asset( 'assets/backend/lib/perfect-scrollbar/css/perfect-scrollbar.min.css' )}}"/>
