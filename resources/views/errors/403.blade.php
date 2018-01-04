11796f927f6a65cdc91031b86e6e74718622ec57
rsqpxlavs <contact.digprix@gmail.com>
2018-01-04 22:51:37 +0530

    a 403 error blade view to check gate authorization

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>403 Unauthorized - {{ config('app.name') }}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <h2>FORBIDDEN - server refuses to authorize</h2>
+</body>
+</html>
