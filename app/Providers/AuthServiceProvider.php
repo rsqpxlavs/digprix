98e1b8155d113bdaff7303d844d19ff8a122b862
rsqpxlavs <contact.digprix@gmail.com>
2018-01-31 13:46:24 +0530

    now broadcasting is possible for both user & admin level

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
+use Auth;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -25,6 +26,16 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
+        //resolving the currently logged in user's instance for pusher authentication
+        Auth::resolveUsersUsing(function ($guard = null) {
+            if( is_null($guard) ){
+                if( Auth::guard('admin')->check()) return Auth::guard('admin')->user();
+                else if( Auth::guard('web')->check()) return Auth::guard('web')->user();
+            }
+            return Auth::guard($guard)->user();
+        });
+
+        //authorizations
         Gate::define('manage_admin_accounts', function ($user) {
             return $user->super_admin || $user->accesslevel->contains('role', 'admin');
         });
